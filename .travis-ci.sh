6db2385f51304645436ce2811f093cdfa6863b0c
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2014-10-22 20:08:46 +0100

    Update travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,9 +1,9 @@
-echo "yes" | sudo add-apt-repository ppa:avsm/ocaml41+opam11
+echo "yes" | sudo add-apt-repository ppa:avsm/ocaml42+opam12
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
-sudo apt-get install libpcre3-dev libssl-dev libsqlite3-dev
+sudo apt-get install libpcre3-dev libssl-dev
 export OPAMYES=1
 opam init
 eval `opam config env`
-opam install batteries eliom safepass oasis
+opam install batteries eliom safepass oasis dbm
 make
