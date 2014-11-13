79874916f77896a4a6c550acddd0ef0a1c7c09ed
Enguerrand Decorne <decorne.en@gmail.com>
2014-11-13 01:46:58 +0000

    Move current file structure for a more clearer distinction between API/Website

--- src/registration.ml
+++
@@ -1,9 +0,0 @@
-open Batteries
-open Eliom_lib.Lwt_ops
-
-let () =
-  Eliom_registration.String.register
-    ~service:Services.test
-    (fun () () ->
-       Lwt.return ("test", "text/plain")
-    )
---
+++ src/site_registration.ml
@@ -0,0 +1,9 @@
+open Batteries
+open Eliom_lib.Lwt_ops
+
+let () =
+  Eliom_registration.Html5.register
+    ~service:Site_services.test
+    (fun () () ->
+       Lwt.return @@ Site_templates.format_page (Eliom_content.Html5.F.pcdata "lol")
+    )
