0d904d63643a41425516fa64e9b1d6175cbd5202
Enguerrand Decorne <decorne.en@gmail.com>
2015-02-10 12:51:45 +0000

    Fix _oasis and add find_user in db_mongodb

--- src/db_mongodb.ml
+++ src/db_mongodb.ml
@@ -23,19 +23,32 @@ let count ?skip ?limit ?(query=Bson.empty) m =
        (Bson.get_element "n"
           d))
 
-let get_user_collection _ =
+let user_collection =
   Mongo.create_local_default "lamaurbain" "users"
 
 let create_user ~username ~password ~email =
-  let m = get_user_collection () in
   let doc =
     empty
     |> Bson.add_element "username" (Bson.create_string username)
     |> Bson.add_element "password" (Bson.create_string password)
     |> Bson.add_element "email" (Bson.create_string email)
-    |> Bson.add_element "id" (Bson.create_int32 @@ Int32.of_int (count m)) in
-  Mongo.insert m [doc];
+    |> Bson.add_element "id" (Bson.create_int32 @@ Int32.of_int (count
+    user_collection)) in
+  Mongo.insert user_collection [doc];
   let open Users in
   {username; password; email}
 
+let find_user id =
+  let query =
+    empty
+    |> Bson.add_element "id" (Bson.create_int32 @@ Int32.of_int id) in
+  let response = Mongo.find_q_one user_collection query in
+  match MongoReply.get_document_list response with
+  | [] -> None
+  | doc::_ ->
+      let username = Bson.get_element "username" doc |> Bson.get_string in
+      let password = Bson.get_element "password" doc |> Bson.get_string in
+      let email = Bson.get_element "email" doc |> Bson.get_string in
+      let open Users in
+      Some {username; password; email}
 
--- src/db_mongodb.mli
+++ src/db_mongodb.mli
@@ -1,1 +1,2 @@
 val create_user : username:string -> password:string -> email:string -> Users.t
+val find_user : int -> Users.t option
