2dc145e3001e3382b889757cff85d499e6ba67c2
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2015-04-16 20:47:35 +0100

    Update the travis file

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,9 +1,18 @@
 echo "yes" | sudo add-apt-repository ppa:avsm/ocaml42+opam12
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
-sudo apt-get install libpcre3-dev libssl-dev
+sudo apt-get install build-essential autoconf autotools-dev automake libtool libpango1.0-dev libcairo2-dev
+sudo apt-get install libpcre3-dev libssl-dev libgdbm-dev libffi-dev
 export OPAMYES=1
 opam init
 eval `opam config env`
-opam install batteries eliom safepass oasis dbm mongo
+opam pin add macaque https://github.com/ocsigen/macaque.git
+opam pin add ocsigenserver https://github.com/ocsigen/ocsigenserver.git#cohttp_rebased
+opam pin add eliom https://github.com/ocsigen/eliom.git#cohttp
+opam install batteries eliom safepass oasis dbm mongo cairo2 macaque monomorphic ppx_deriving_yojson ctypes ctypes-foreign batteries
+git clone git://git.code.sf.net/p/libosmscout/code libosmscout
+cd libosmscout
+make full
+make install
+cd ..
 make
