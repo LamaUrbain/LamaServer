6ce78f629cbce47f5106ebb2520d1f3591f106d3
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2015-04-16 22:23:31 +0100

    Fix the travis script

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -7,9 +7,10 @@ export OPAMYES=1
 opam init
 eval `opam config env`
 opam pin add macaque https://github.com/ocsigen/macaque.git
+opam install ppx_deriving_yojson
 opam pin add ocsigenserver https://github.com/ocsigen/ocsigenserver.git#cohttp_rebased
 opam pin add eliom https://github.com/ocsigen/eliom.git#cohttp
-opam install batteries eliom safepass oasis dbm mongo cairo2 macaque monomorphic ppx_deriving_yojson ctypes ctypes-foreign batteries
+opam install batteries eliom safepass oasis dbm mongo cairo2 macaque monomorphic ctypes ctypes-foreign batteries
 git clone https://github.com/LamaUrbain/libosmscout
 cd libosmscout
 make full-install
