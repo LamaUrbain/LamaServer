1dfaf8f93da539aa9c42f8488985ae2d770e9f5c
Enguerrand Decorne <decorne.en@gmail.com>
2015-05-25 02:28:38 +0200

    Add constraints and created field

--- createdb.sql
+++ createdb.sql
@@ -4,17 +4,34 @@ CREATE USER lamaurbain PASSWORD 'lamaurbain';
 CREATE DATABASE lamaurbain OWNER lamaurbain ENCODING 'UTF8';
 \connect lamaurbain
 
-CREATE TABLE users_table (
-  username text NOT NULL,
-  password text NOT NULL,
-  email text NOT NULL,
-  id integer PRIMARY KEY
+CREATE DOMAIN dom_name TEXT CHECK (
+  LENGTH(VALUE) > 1 AND
+  LENGTH(VALUE) < 200
 );
+
+CREATE DOMAIN dom_mail TEXT CHECK (
+  LENGTH(VALUE) > 3 AND
+  LENGTH(VALUE) < 254
+);
+
+CREATE DOMAIN dom_username TEXT CHECK (
+  LENGTH(VALUE) < 100
+);
+
+CREATE TABLE users_table (
+  username dom_username NOT NULL,
+  password text NOT NULL,
+  email dom_mail NOT NULL,
+  id integer PRIMARY KEY,
+  created date NOT NULL default CURRENT_DATE
+);
+
 ALTER TABLE users_table OWNER TO lamaurbain;
 
 CREATE TABLE auth_table (
   token text NOT NULL,
   owner integer NOT NULL,
+  created date NOT NULL default CURRENT_DATE,
   PRIMARY KEY token
 );
 
@@ -22,15 +39,17 @@ CREATE TABLE coord_table (
   id integer PRIMARY KEY,
   latitude real NOT NULL,
   longitude real NOT NULL,
+  created date NOT NULL default CURRENT_DATE,
   address text
 );
 
 CREATE TABLE itinerary_table (
   id integer PRIMARY KEY,
   owner integer,
-  name text,
+  name dom_name,
   favorite boolean,
   departure int NOT NULL,
+  created date NOT NULL default CURRENT_DATE,
   destinations int[]
 );
 
