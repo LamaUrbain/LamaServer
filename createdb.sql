af0aa535a859c8b0327df8d8d0b00929c4505bc5
Enguerrand Decorne <decorne.en@gmail.com>
2015-05-25 02:13:45 +0200

    Add new tables into postgresql

--- createdb.sql
+++ createdb.sql
@@ -12,5 +12,27 @@ CREATE TABLE users_table (
 );
 ALTER TABLE users_table OWNER TO lamaurbain;
 
+CREATE TABLE auth_table (
+  token text NOT NULL,
+  owner integer NOT NULL,
+  PRIMARY KEY token
+);
+
+CREATE TABLE coord_table (
+  id integer PRIMARY KEY,
+  latitude real NOT NULL,
+  longitude real NOT NULL,
+  address text
+);
+
+CREATE TABLE itinerary_table (
+  id integer PRIMARY KEY,
+  owner integer,
+  name text,
+  favorite boolean,
+  departure int NOT NULL,
+  destinations int[]
+);
+
 CREATE SEQUENCE users_id_seq;
 ALTER SEQUENCE users_id_seq OWNER TO lamaurbain;
