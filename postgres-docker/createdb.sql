1304902ba33a5a8cf53874ff8483d77b7fc42bc5
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2015-05-31 19:27:20 +0100

    Improve and fix createdb.sql

--- postgres-docker/createdb.sql
+++ postgres-docker/createdb.sql
@@ -8,15 +8,18 @@ CREATE DOMAIN dom_name TEXT CHECK (
   LENGTH(VALUE) > 1 AND
   LENGTH(VALUE) < 200
 );
+ALTER DOMAIN dom_name OWNER TO lamaurbain;
 
 CREATE DOMAIN dom_mail TEXT CHECK (
   LENGTH(VALUE) > 3 AND
   LENGTH(VALUE) < 254
 );
+ALTER DOMAIN dom_mail OWNER TO lamaurbain;
 
 CREATE DOMAIN dom_username TEXT CHECK (
   LENGTH(VALUE) < 100
 );
+ALTER DOMAIN dom_username OWNER TO lamaurbain;
 
 CREATE TABLE users_table (
   username dom_username NOT NULL,
@@ -25,22 +28,23 @@ CREATE TABLE users_table (
   id integer PRIMARY KEY,
   created date NOT NULL default CURRENT_DATE
 );
-
 ALTER TABLE users_table OWNER TO lamaurbain;
 
 CREATE TABLE auth_table (
   token text NOT NULL PRIMARY KEY,
   owner integer NOT NULL,
-  created date NOT NULL default CURRENT_DATE,
+  created date NOT NULL DEFAULT CURRENT_DATE,
 );
+ALTER TABLE auth_table OWNER TO lamaurbain;
 
 CREATE TABLE coord_table (
   id integer PRIMARY KEY,
   latitude real NOT NULL,
   longitude real NOT NULL,
-  created date NOT NULL default CURRENT_DATE,
+  created date NOT NULL DEFAULT CURRENT_DATE,
   address text
 );
+ALTER TABLE coord_table OWNER TO lamaurbain;
 
 CREATE TABLE itinerary_table (
   id integer PRIMARY KEY,
@@ -48,9 +52,10 @@ CREATE TABLE itinerary_table (
   name dom_name,
   favorite boolean,
   departure int NOT NULL,
-  created date NOT NULL default CURRENT_DATE,
+  created date NOT NULL DEFAULT CURRENT_DATE,
   destinations int[]
 );
+ALTER TABLE itinerary_table OWNER TO lamaurbain;
 
 CREATE SEQUENCE users_id_seq;
 ALTER SEQUENCE users_id_seq OWNER TO lamaurbain;
