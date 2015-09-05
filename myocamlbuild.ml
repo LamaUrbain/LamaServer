febab9d99960331758122557b347247f2abb46db
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2015-09-05 18:56:43 +0200

    Remove the client and server side eliom modules

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,17 +1,4 @@
 (* OASIS_START *)
 (* OASIS_STOP *)
 
-module M = Ocamlbuild_eliom.Make(struct
-  let client_dir = "client"
-  let server_dir = "server"
-  let type_dir = "type"
-end)
-
-let () =
-  dispatch
-    (fun hook ->
-       dispatch_default hook;
-       M.dispatcher
-         ~oasis_executables:["src/client/lama_server.byte"]
-         hook;
-    )
+let () = dispatch dispatch_default
