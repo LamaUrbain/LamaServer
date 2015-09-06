9a49ebdf1e36254f71afae867f48b3c40fb07c52
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2015-09-06 17:28:20 +0200

    Add the possibility of creating switching database using the config file

--- src/config.mli
+++ src/config.mli
@@ -1,6 +1,22 @@
+type postgres =
+  { host : string
+  ; port : int option
+  ; database : string
+  ; user : string
+  ; password : string
+  }
+
+type mongodb =
+  { host : string
+  ; port : int
+  ; name : string
+  ; collection : string
+  }
+
+type database =
+  | Postgres of postgres
+  | MongoDB of mongodb
+
 val map : string
 val style : string
-val host : string
-val database : string
-val user : string
-val password : string
+val database : database
