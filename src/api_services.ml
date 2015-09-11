dc3c62da066fd6d2fda66ca37011ab6ee7fcf130
Enguerrand Decorne <decorne.en@gmail.com>
2015-09-11 10:55:09 +0200

    Add any in all services.

--- src/api_services.ml
+++ src/api_services.ml
@@ -32,7 +32,7 @@ let read_raw_content ?(length = 4096) raw_content =
   let content_stream = Ocsigen_stream.get raw_content in
   Ocsigen_stream.string_of_stream length content_stream
 
-let user_get_handler (id_opt, (search_pattern, sponsored)) () =
+let user_get_handler (id_opt, (search_pattern, (sponsored, _))) () =
   match id_opt, sponsored with
   | None, None
   | None, Some false->
@@ -90,7 +90,7 @@ let wrap_errors f = function
   | `Ok x -> f x
   | `Error x -> send_error ~code:400 ("Provided JSON is not valid: " ^ x)
 
-let user_post_handler _ (username ,(password, (email, sponsor)))  =
+let user_post_handler _ (username ,(password, (email, (sponsor, _))))  =
     let user = {username;password;email} in
        wrap_errors
          (fun user ->
@@ -102,7 +102,7 @@ let user_post_handler _ (username ,(password, (email, sponsor)))  =
             >>= fun u -> send_success ~content:(Yojson.Safe.to_string (Users.response_to_yojson (Users.to_response u))) ()
          ) (`Ok user)
 
-let session_post_handler _ (username, password)  =
+let session_post_handler _ (username, (password, any))  =
   D.find_user_username username >>= fun u ->
   match u with
     | Some user_t when user_t.password <> password ->
@@ -120,7 +120,7 @@ let session_post_handler _ (username, password)  =
           ~code:404
           ("User not found")
 
-let session_get_handler token_opt () =
+let session_get_handler (token_opt, any) () =
   match token_opt with
   | None ->
     send_error ~code:404 "Missing id"
@@ -138,7 +138,7 @@ let session_get_handler token_opt () =
           ("User not found")
     )
 
-let users_delete_handler (id, token) _ =
+let users_delete_handler (id, (token, any)) _ =
   D.find_user_username id >>= fun u ->
   match u with
     | Some user_t ->
@@ -152,7 +152,7 @@ let users_delete_handler (id, token) _ =
           ~code:404
           ("User not found")
 
-let users_put_handler (id, (token, (username, (email, (password, sponsor))))) _ =
+let users_put_handler (id, (token, (username, (email, (password, (sponsor, any)))))) _ =
   D.find_user_username id >>= fun u ->
   match u with
     | Some user_t ->
@@ -166,7 +166,7 @@ let users_put_handler (id, (token, (username, (email, (password, sponsor))))) _ 
           ~code:404
           ("User not found")
 
-let sessions_delete_handler token _ =
+let sessions_delete_handler (token, _) _ =
   wrap_errors
     (fun _ ->
      D.delete_session token
@@ -209,7 +209,7 @@ let () =
 
   let dummy_handler _ _ = Eliom_registration.String.send ~code:201 ("", "") in
 
-  let itinerary_post_handler _ (departure, (departure_address, (destination_address, (favorite, (destination, (name, token)))))) =
+  let itinerary_post_handler _ (departure, (departure_address, (destination_address, (favorite, (destination, (name, (token, _))))))) =
     let destination = BatOption.map (coord_of_param destination_address) destination in
     let departure = coord_of_param departure_address departure in
     let coords : Request_data.itinerary_creation = {destination; departure; favorite; name} in
@@ -224,12 +224,12 @@ let () =
     (`Ok coords)
   in
 
-  let itinerary_get_handler (id, token) () =
+  let itinerary_get_handler (id, (token, any)) () =
     Itinerary.get id >>= fun itinerary ->
     send_json ~code:200 (Yojson.Safe.to_string (Result_data.itinerary_to_yojson itinerary))
   in
 
-  let itinerary_put_handler (id, (departure, (departure_address, (favorite, (name, token))))) _ =
+  let itinerary_put_handler (id, (departure, (departure_address, (favorite, (name, (token, any)))))) _ =
     check_itinerary_ownership id token >>= fun it ->
     match it with
     | Answer _ -> (
@@ -247,7 +247,7 @@ let () =
     | Error e -> send_error ~code:403 e
   in
 
-  let destinations_post_handler (id, ()) (destination, (destination_address, (position, token))) =
+  let destinations_post_handler (((id : int32), ()), _) (destination, (destination_address, (position, (token, _)))) =
     check_itinerary_ownership id token >>= fun it ->
     match it with
     | Answer _ -> (
@@ -265,7 +265,7 @@ let () =
     | Error e -> send_error ~code:403 e
   in
 
-  let destinations_put_handler ((id, ((), pos)), (destination, (destination_address, (position, token)))) _ =
+  let destinations_put_handler ((id, ((), pos)), (destination, (destination_address, (position, (token, any))))) _ =
     check_itinerary_ownership id token >>= fun it ->
     match it with
     | Answer _ -> (
@@ -284,7 +284,7 @@ let () =
     | Error e -> send_error ~code:403 e
   in
 
-  let delete_handler (get, token) delete  =
+  let delete_handler (get, (token, any)) delete  =
     match get with
    | [id; "destinations"; position] ->
         let id = Int32.of_string id in
@@ -306,13 +306,13 @@ let () =
     Itinerary.get_image ~x ~y ~z id >>= fun image ->
     Eliom_registration.String.send (image, "image/png") in
 
-  let coords_get_handler (id, ((), z)) _ =
+  let coords_get_handler ((id, (_ , z )), _) _ =
     let zoom = Itinerary.Zoomlevel.create z in
     Itinerary.get_coordinates ~zoom id >>= fun coords ->
     send_json ~code:200 (Yojson.Safe.to_string (Itinerary.coordinate_list_to_yojson coords))
   in
 
-  let get_handler_with_params (search, (owner, (favorite, ordering))) () =
+  let get_handler_with_params (search, (owner, (favorite, (ordering, any)))) () =
     let params =
       { Request_data.search
       ; owner
@@ -327,7 +327,7 @@ let () =
   let service =
     Eliom_service.Http.delete_service
       ~path:["itineraries"]
-      ~get_params:(suffix_prod (all_suffix "params") (opt (string "token")))
+      ~get_params:(suffix_prod (all_suffix "params") (opt (string "token") ** any))
       ()
   in
   Eliom_registration.Any.register ~service delete_handler;
@@ -335,7 +335,7 @@ let () =
   let service =
     Eliom_service.Http.service
       ~path:["itineraries"]
-      ~get_params:(suffix_prod (int32 "id") (neopt (string "token")))
+      ~get_params:(suffix_prod (int32 "id") (neopt (string "token") ** any))
       ()
   in
   Eliom_registration.Any.register ~service itinerary_get_handler;
@@ -346,7 +346,7 @@ let () =
       ~get_params:(opt (string "search")
                    ** opt (string "owner")
                    ** opt (bool "favorite")
-                   ** opt (string "ordering"))
+                   ** opt (string "ordering") ** any)
       ()
   in
   Eliom_registration.Any.register ~service get_handler_with_params;
@@ -361,6 +361,7 @@ let () =
                     ** opt (string "destination")
                     ** opt (string "name")
                     ** opt (string "token")
+		    ** any
 		   )
      ()
   in
@@ -376,6 +377,7 @@ let () =
                       ** opt (bool "favorite")
                       ** opt (string "name")
                       ** opt (string "token")
+		      ** any
                      )
                   )
       ()
@@ -386,7 +388,7 @@ let () =
   let service =
     Eliom_service.Http.service
       ~path:["itineraries"]
-      ~get_params:(suffix (int32 "id" ** suffix_const "destinations"))
+      ~get_params:(suffix_prod (int32 "id" ** suffix_const "destinations") (any))
       () in
   Eliom_registration.Any.register ~service dummy_handler;
 
@@ -397,6 +399,7 @@ let () =
 		    ** opt (string "destination_address")
                     ** opt (int "position")
                     ** opt (string "token")
+		    ** any
                   )
      ()
   in
@@ -406,7 +409,7 @@ let () =
       ~path:["itineraries"]
       ~get_params:(suffix_prod
                      (int32 "id" ** suffix_const "destinations" ** int "pos")
-                     (opt (string "destination") ** opt (string "destination_address") ** opt (int "position") ** opt (string "token"))
+                     (opt (string "destination") ** opt (string "destination_address") ** opt (int "position") ** opt (string "token") ** any)
                   )
       ()
   in
@@ -430,17 +433,17 @@ let () =
   let service =
     Eliom_service.Http.service
       ~path:["itineraries"]
-      ~get_params:(suffix (
+      ~get_params:(suffix_prod (
                    int32 "id" **
                    suffix_const "coordinates" **
-                   int "z"))
+                   int "z") (any))
       () in
   Eliom_registration.Any.register ~service coords_get_handler;
 
   let service =
     Eliom_service.Http.service
       ~path:["users"]
-      ~get_params:(suffix_prod (neopt (string "id")) (neopt (string "search") ** neopt (bool "sponsored")))
+      ~get_params:(suffix_prod (neopt (string "id")) (neopt (string "search") ** neopt (bool "sponsored") ** any))
       () in
   Eliom_registration.Any.register ~service user_get_handler;
 
@@ -450,7 +453,7 @@ let () =
       ~post_params:(string "username"
                     ** (string "password")
                     ** (string "email")
-		    ** (bool "sponsor"))
+		    ** (bool "sponsor") ** any)
       ()
   in
   Eliom_registration.Any.register ~service user_post_handler;
@@ -458,7 +461,7 @@ let () =
   let service =
     Eliom_service.Http.service
       ~path:["sessions"]
-      ~get_params:(suffix (neopt (string "token")))
+      ~get_params:(suffix_prod (neopt (string "token")) (any))
       () in
   Eliom_registration.Any.register ~service session_get_handler;
 
@@ -466,7 +469,7 @@ let () =
     Eliom_service.Http.post_service
       ~fallback:service
       ~post_params:(string "username"
-                    ** (string "password"))
+                    ** (string "password") ** any)
       ()
   in
   Eliom_registration.Any.register ~service session_post_handler;
@@ -474,7 +477,7 @@ let () =
   let service =
     Eliom_service.Http.delete_service
       ~path:["users"]
-      ~get_params:(suffix_prod (string "id") (string "token"))
+      ~get_params:(suffix_prod (string "id") (string "token" ** any))
       ()
   in
   Eliom_registration.Any.register ~service users_delete_handler;
@@ -482,7 +485,7 @@ let () =
   let service =
     Eliom_service.Http.delete_service
       ~path:["sessions"]
-      ~get_params:(suffix (string "token"))
+      ~get_params:(suffix_prod (string "token") (any))
       ()
   in
   Eliom_registration.Any.register ~service sessions_delete_handler;
@@ -495,7 +498,8 @@ let () =
 				  neopt (string "email") **
 				    neopt (string "username") **
 				      neopt (string "password") **
-					neopt (bool "sponsor")))
+					neopt (bool "sponsor") **
+					  any))
       ()
   in
   Eliom_registration.Any.register ~service users_put_handler;
