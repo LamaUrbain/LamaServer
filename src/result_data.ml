ea2e1509a309d0b45d42114947ba812afa300f93
Gregoire Lionnet <gregoirelionnet@gmail.com>
2015-11-01 16:17:38 +0100

    Add incident manipulation type in result/request data.

--- src/result_data.ml
+++ src/result_data.ml
@@ -7,3 +7,12 @@ type itinerary =
   ; departure : Request_data.coord
   ; destinations : Request_data.coord list
   } [@@deriving yojson]
+
+type incident =
+  {
+    id : int32;
+    name : string option;
+    begin_ : string;
+    end_ : string;
+    position : Request_data.cood;
+  } [@@deriving yojson]
