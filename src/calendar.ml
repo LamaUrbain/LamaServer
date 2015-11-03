7cdcaca5a0d612c2408174e8ad0d3d6c0acb97aa
Enguerrand Decorne <decorne.en@gmail.com>
2015-11-03 17:42:21 +0100

    Now parse the date with calendarlib to yojson

--- src/calendar.ml
+++ src/calendar.ml
@@ -1,5 +1,12 @@
 type t = CalendarLib.Calendar.t
 
-let to_yojson (date : t) = Yojson.Safe.from_string "lol"
+let to_yojson (date : t) =
+  let date_string = CalendarLib.Printer.Calendar.to_string date in
+  Yojson.Safe.from_string date_string
 
-let of_yojson (date : Yojson.Safe.json) = `Ok (CalendarLib.Calendar.now ())
+let of_yojson (date : Yojson.Safe.json) =
+  try
+    let date_string = Yojson.Safe.to_string date in
+    `Ok (CalendarLib.Printer.Calendar.from_string date_string)
+  with
+  | _ -> `Error "Error while parsing date"
